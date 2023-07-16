3043f6735cb30e58fd077723b592e10cac9e6084
Gianluca Mazzeo <gianlucamazzeo@gmail.com>
2023-07-16 18:07:50 +0200

    added dev config

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    "<googleusercontentId>",
+  googleClientSecret: "<secretContent>",
+  mongoURI:
+    "mongodb+srv://<Username>:<password>@<clusterMongodb>",
+  cookieKey: "123123123",
+};
+
